73a65fe4965b29eb929b88e360b0ab301f0222ba
tmurphy3 <tim.murphy@uconn.edu>
2020-11-11 15:46:50 -0500

    change port

--- server.js
+++ server.js
@@ -82,6 +82,6 @@ app.delete("/users/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
