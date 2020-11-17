8be77e54a41a8140a58c3f9aed400b02b48fd78e
tmurphy3 <tim.murphy@uconn.edu>
2020-11-17 17:10:33 -0500

    fix route

--- routes.js
+++ routes.js
@@ -116,7 +116,7 @@ routes.get("/user/posts", async (req, res) => {
     const client = await connection.connect();
     const { user_id } = req.headers;
     const usersPosts = await client.query(
-      "select p.*, s.subreddit_title, u.email from posts_table p join subreddits_table s on p.subreddit_id = s.subreddit_id join users_table u on u.user_id = s.user_id WHERE p.user_id = $1",
+      "select p.*, s.subreddit_title, u.email from posts_table p join subreddits_table s on p.subreddit_id = s.subreddit_id join users_table u on u.user_id = p.user_id WHERE p.user_id = $1",
       [user_id]
     );
     res.json(usersPosts.rows);
