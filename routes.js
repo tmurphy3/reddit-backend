7b3013cdfe81ae4d7342ba6fff44c977c6636735
tmurphy3 <tim.murphy@uconn.edu>
2020-12-07 12:15:04 -0500

    fix bug

--- routes.js
+++ routes.js
@@ -145,7 +145,7 @@ routes.get("/subreddits/:id", async (req, res) => {
     const client = await connection.connect();
     const { id } = req.params;
     const user = await client.query(
-      "select * subreddits_table where subreddit_id = $1",
+      "select * from subreddits_table where subreddit_id = $1",
       [id]
     );
     res.json(user.rows);
@@ -155,6 +155,20 @@ routes.get("/subreddits/:id", async (req, res) => {
   }
 });
 
+routes.get("/subredditsx/:id", async (req, res) => {
+  try {
+    const client = await connection.connect();
+    const { id } = req.params;
+    const user = await client.query(
+      "select COUNT(p.subreddit_id), s.subreddit_title, s.subreddit_id from posts_table p join subreddits_table s on s.subreddit_id = p.subreddit_id where s.subreddit_id = $1 GROUP BY p.subreddit_id",
+      [id]
+    );
+    res.json(user.rows);
+    client.release();
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 // popular subreddits
 routes.get("/trending", async (req, res) => {
   try {
