5e18676657ee215ceee87274d9b6bfe81d642d90
tmurphy3 <tim.murphy@uconn.edu>
2020-12-19 15:04:26 -0500

    organize crud routes

--- routes.js
+++ routes.js
@@ -29,6 +29,7 @@ routes.get("/login", async (req, res) => {
   }
 });
 
+// crud routes
 // users
 // get all users
 routes.get("/users", async (req, res) => {
@@ -110,22 +111,6 @@ routes.delete("/users/:id", async (req, res) => {
   }
 });
 
-// all posts by user
-routes.get("/user/posts", async (req, res) => {
-  try {
-    const client = await connection.connect();
-    const { user_id } = req.headers;
-    const usersPosts = await client.query(
-      "select p.*, s.subreddit_title, u.email from posts_table p join subreddits_table s on p.subreddit_id = s.subreddit_id join users_table u on u.user_id = p.user_id WHERE p.user_id = $1",
-      [user_id]
-    );
-    res.json(usersPosts.rows);
-    client.release();
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
 // subreddits
 //get all subreddits
 routes.get("/subreddits", async (req, res) => {
@@ -154,19 +139,6 @@ routes.get("/subreddits/:id", async (req, res) => {
     console.error(err.message);
   }
 });
-// popular subreddits
-routes.get("/trending", async (req, res) => {
-  try {
-    const client = await connection.connect();
-    const posts = await client.query(
-      "select COUNT(p.subreddit_id), s.subreddit_title, s.subreddit_id from posts_table p join subreddits_table s on s.subreddit_id = p.subreddit_id GROUP BY s.subreddit_id order by COUNT(p.subreddit_id) desc nulls last limit 6"
-    );
-    res.json(posts.rows);
-    client.release();
-  } catch (err) {
-    console.error(err.message);
-  }
-});
 
 // create a subreddit
 routes.post("/subreddits", async (req, res) => {
@@ -183,37 +155,6 @@ routes.post("/subreddits", async (req, res) => {
       [subreddit_title, subreddit_image, subreddit_content, user_id]
     );
     res.json(newSubreddit.rows[0]);
-    client.release();
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-// lists all posts in subreddit
-routes.get("/subreddit/posts", async (req, res) => {
-  try {
-    const client = await connection.connect();
-    const { subreddit_id } = req.headers;
-    const posts = await client.query(
-      "select p.*, s.subreddit_title, s.subreddit_content, s.subreddit_image, u.email from posts_table p join users_table u on p.user_id = u.user_id join subreddits_table s on s.subreddit_id = p.subreddit_id where p.subreddit_id = $1",
-      [subreddit_id]
-    );
-    res.json(posts.rows);
-    client.release();
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-routes.get("/post/:id", async (req, res) => {
-  try {
-    const client = await connection.connect();
-    const { id } = req.params;
-    const comments = await client.query(
-      "select p.*, COUNT(c.post_id) as comments from posts_table p join comments_table c on p.post_id = c.post_id where p.post_id = $1 group by p.post_id",
-      [id]
-    );
-    res.json(comments.rows);
     client.release();
   } catch (err) {
     console.error(err.message);
@@ -269,23 +210,6 @@ routes.post("/posts", async (req, res) => {
   }
 });
 
-// update a post (for upvotes)
-routes.put("/posts/:id", async (req, res) => {
-  try {
-    const client = await connection.connect();
-    const { id } = req.params;
-    const { post_upvotes } = req.body;
-    const updatedPost = await client.query(
-      "UPDATE posts_table SET post_upvotes = $1 WHERE post_id = $2",
-      [post_upvotes, id]
-    );
-    res.json("post was updated");
-    client.release();
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
 // update a post (for editing info)
 routes.put("/post/:id", async (req, res) => {
   try {
@@ -313,36 +237,6 @@ routes.delete("/posts/:id", async (req, res) => {
       [id]
     );
     res.json("Post was deleted");
-    client.release();
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-// lists all comments in post
-routes.get("/subreddit/post/comments", async (req, res) => {
-  try {
-    const client = await connection.connect();
-    const { post_id } = req.headers;
-    const comments = await client.query(
-      "select c.*, u.email from comments_table c join users_table u on c.user_id = u.user_id and c.post_id = $1 order by c.comment_upvotes desc nulls last",
-      [post_id]
-    );
-    res.json(comments.rows);
-    client.release();
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-// popular posts
-routes.get("/popular", async (req, res) => {
-  try {
-    const client = await connection.connect();
-    const popularPosts = await client.query(
-      "select p.*, s.subreddit_title, u.email from posts_table p join subreddits_table s on p.subreddit_id = s.subreddit_id join users_table u on u.user_id = p.user_id order by p.post_upvotes desc nulls last limit 15"
-    );
-    res.json(popularPosts.rows);
     client.release();
   } catch (err) {
     console.error(err.message);
@@ -405,4 +299,113 @@ routes.delete("/comments/:id", async (req, res) => {
   }
 });
 
+// Joins
+// all posts by user
+routes.get("/user/posts", async (req, res) => {
+  try {
+    const client = await connection.connect();
+    const { user_id } = req.headers;
+    const usersPosts = await client.query(
+      "select p.*, s.subreddit_title, u.email from posts_table p join subreddits_table s on p.subreddit_id = s.subreddit_id join users_table u on u.user_id = p.user_id WHERE p.user_id = $1",
+      [user_id]
+    );
+    res.json(usersPosts.rows);
+    client.release();
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// popular subreddits
+routes.get("/trending", async (req, res) => {
+  try {
+    const client = await connection.connect();
+    const posts = await client.query(
+      "select COUNT(p.subreddit_id), s.subreddit_title, s.subreddit_id from posts_table p join subreddits_table s on s.subreddit_id = p.subreddit_id GROUP BY s.subreddit_id order by COUNT(p.subreddit_id) desc nulls last limit 6"
+    );
+    res.json(posts.rows);
+    client.release();
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// lists all posts in subreddit
+routes.get("/subreddit/posts", async (req, res) => {
+  try {
+    const client = await connection.connect();
+    const { subreddit_id } = req.headers;
+    const posts = await client.query(
+      "select p.*, s.subreddit_title, s.subreddit_content, s.subreddit_image, u.email from posts_table p join users_table u on p.user_id = u.user_id join subreddits_table s on s.subreddit_id = p.subreddit_id where p.subreddit_id = $1",
+      [subreddit_id]
+    );
+    res.json(posts.rows);
+    client.release();
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// find amount of comments in posts
+routes.get("/post/:id", async (req, res) => {
+  try {
+    const client = await connection.connect();
+    const { id } = req.params;
+    const comments = await client.query(
+      "select p.*, COUNT(c.post_id) as comments from posts_table p join comments_table c on p.post_id = c.post_id where p.post_id = $1 group by p.post_id",
+      [id]
+    );
+    res.json(comments.rows);
+    client.release();
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// update a post (for upvotes)
+routes.put("/posts/:id", async (req, res) => {
+  try {
+    const client = await connection.connect();
+    const { id } = req.params;
+    const { post_upvotes } = req.body;
+    const updatedPost = await client.query(
+      "UPDATE posts_table SET post_upvotes = $1 WHERE post_id = $2",
+      [post_upvotes, id]
+    );
+    res.json("post was updated");
+    client.release();
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// find all comments in post
+routes.get("/subreddit/post/comments", async (req, res) => {
+  try {
+    const client = await connection.connect();
+    const { post_id } = req.headers;
+    const comments = await client.query(
+      "select c.*, u.email from comments_table c join users_table u on c.user_id = u.user_id and c.post_id = $1 order by c.comment_upvotes desc nulls last",
+      [post_id]
+    );
+    res.json(comments.rows);
+    client.release();
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// popular posts
+routes.get("/popular", async (req, res) => {
+  try {
+    const client = await connection.connect();
+    const popularPosts = await client.query(
+      "select p.*, s.subreddit_title, u.email from posts_table p join subreddits_table s on p.subreddit_id = s.subreddit_id join users_table u on u.user_id = p.user_id order by p.post_upvotes desc nulls last limit 15"
+    );
+    res.json(popularPosts.rows);
+    client.release();
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 module.exports = routes;
